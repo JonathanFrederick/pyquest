1590759719d46eef7b3f685ed271d67c14bddb44
Jonathan Frederick <jrfreder@gmail.com>
2016-06-14 16:43:49 -0400

    testing for player creation with default name

--- test_models.py
+++ test_models.py
@@ -5,3 +5,7 @@ class TestPlayer:
     def test_player_creation(self):
         pal = Player("Paladin")
         assert pal.name == "Paladin"
+
+    def test_player_default_name(self):
+        brave = Player()
+        assert brave.name == "Brave Knight"
