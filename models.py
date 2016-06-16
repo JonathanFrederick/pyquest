f4e5c421190774b51abf95836792d99264357905
Jonathan Frederick <jrfreder@gmail.com>
2016-06-15 22:46:01 -0400

    method for assigning a player class

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 class Player():
-    def __init__(self, name="Brave Knight"):
+    def __init__(self, name="Brave Hero"):
         self.name = name
+
+    def get_class(self, p_class):
+        p_class = p_class.lower()
+        if p_class in ['warrior', 'mage', 'archer']:
+            self.p_class = p_class
+            return True
+        else:
+            return False
