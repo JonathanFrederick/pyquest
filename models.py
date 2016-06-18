7f57b4ef2509ce7caa4339400416816bbe583b7b
Jonathan Frederick <jrfreder@gmail.com>
2016-06-17 23:07:37 -0400

    added level and experience default values

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 class Player():
     def __init__(self, name="Brave Hero"):
         self.name = name
+        self.level = 1
+        self.exp = 0
 
     def get_class(self, p_class):
         p_class = p_class.lower()
-        if p_class in ['warrior', 'mage', 'archer']:
+        if p_class in ['warrior', 'mage', 'archer', 'archaeologist']:
             self.p_class = p_class
             return True
         else:
